e42516e5b31b66b64eb930e4cec1e099b0862777
angeltsvetkov <mrproper86@gmail.com>
2014-10-05 19:20:09 +0300

    Update messaging mechanism

--- sample/sample.js
+++ sample/sample.js
@@ -13,9 +13,13 @@ window.onload = function() {
     settings.closeOnComplete = true;
 
     //Init ExtAPI object and provide a message processing callback
-    var executor = new ExtAPI(generateId(), settings, rxMessage);
+    var executor1 = new ExtAPI(1, settings, rxMessage);
+    var executor2 = new ExtAPI(2, settings, rxMessage);
+    var executor3 = new ExtAPI(3, settings, rxMessage);
 
-    executeCode();
+    executeCode1();
+    executeCode2();
+    executeCode3();
     // //Check whether the extension is installed
     // if (typeof(executor.version()) === "undefined") {
     //     textContent.innerHTML = "<p>Please load the chrome extension first! If you don't know how - <a href='https://developer.chrome.com/extensions/getstarted#unpacked' target='_blank'>check this out</a></p>"
@@ -26,16 +30,32 @@ window.onload = function() {
 
     // }
 
-    function executeCode() {
-        executor.open("http://qaagent.com/ide/test.html", settings);
-        executor.execute("*", "$('#test1input').val('test')", "Enter text");
-        executor.execute("*", "$('body > div > button').click()", "Click the button");
-        executor.execute("*", "$('#test1label').hasValue('test')", "Verify label value");
-        executor.end();
+    function executeCode1() {
+        executor1.open("http://qaagent.com/ide/test.html", settings);
+        executor1.execute("*", "$('#test1input').val('test')", "Enter text");
+        executor1.execute("*", "$('body > div > button').click()", "Click the button");
+        executor1.execute("*", "$('#test1label').hasValue('test')", "Verify label value");
+        executor1.end();
+    }
+
+    function executeCode2() {
+        executor2.open("http://qaagent.com/ide/test.html", settings);
+        executor2.execute("*", "$('#test1input').val('test')", "Enter text");
+        executor2.execute("*", "$('body > div > button').click()", "Click the button");
+        executor2.execute("*", "$('#test1label').hasValue('test')", "Verify label value");
+        executor2.end();
+    }
+
+    function executeCode3() {
+        executor3.open("http://qaagent.com/ide/test.html", settings);
+        executor3.execute("*", "$('#test1input').val('test')", "Enter text");
+        executor3.execute("*", "$('body > div > button').click()", "Click the button");
+        executor3.execute("*", "$('#test1label').hasValue('test')", "Verify label value");
+        executor3.end();
     }
 
     function rxMessage(message) {
-        textContent.innerHTML += "<p>Your code execution was completed successfuly! Action: <b>" + message.text + "</b></p>";
+        textContent.innerHTML += "<p><b>" + message.tcid + "</b>Your code execution was completed successfuly! Action: <b>" + message.text + "</b></p>";
     }
 
     function generateId() {
